19daa4bc0418a6cc419a65fe3f3a7ddaac3127b6
madmuv <d.kritaisong@gmail.com>
2024-01-26 13:41:59 +0700

    - added datepicker

--- app/src/main/java/ui/goal/GoalFragment.kt
+++ app/src/main/java/ui/goal/GoalFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.donyawan.salehere.databinding.FragmentGoalBinding
 import java.util.Calendar
@@ -33,11 +34,18 @@ class GoalFragment : Fragment() {
 
             val dialog = DatePickerDialog(
                 requireContext(),
-                { p0, p1, p2, p3 ->  },
-                calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                { p0, p1, p2, p3 ->
+                    binding.etSelectDate.setText("${p0.dayOfMonth}-${p0.month + 1}-${p0.year}")
+                },
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
                 calendar.get(Calendar.DAY_OF_MONTH)
             )
             dialog.show()
+        }
+
+        binding.goalTypeRadio.setOnCheckedChangeListener { radioGroup, i ->
+            Toast.makeText(requireContext(), "selected $i", Toast.LENGTH_SHORT).show()
         }
     }
 
